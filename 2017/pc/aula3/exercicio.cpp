215eb1c955dc64a036dd3e5e9406fce47fdb5d65
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-08-29 20:30:42 -0300

    Exercício completo

--- 2017/pc/aula3/exercicio.cpp
+++ 2017/pc/aula3/exercicio.cpp
@@ -4,10 +4,27 @@
 using namespace std;
 
 typedef struct {
-    String nome;
-    String cpf;
+    string nome;
+    string cpf;
     float saldo;
 } Conta;
+
+void init_conta( Conta &conta ) {
+    cout << "Nome:";
+    cin >> conta.nome;
+
+    cout << "CPF:";
+    cin >> conta.cpf;
+
+    cout << "SALDO:";
+    cin >> conta.saldo;
+}
+
+void print_conta( Conta &conta ) {
+    cout << "NOME: " << conta.nome << '\n';
+    cout << "CPF: " << conta.cpf << '\n';
+    cout << "SALDO: " << conta.saldo << '\n';
+}
 
 float get_saldo( Conta &conta ) {
     return conta.saldo;
@@ -20,6 +37,7 @@ int main() {
     Conta conta;
 
     init_conta( conta );
+    print_conta( conta );
 
     return 0;
 }
