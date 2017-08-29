d3dd310602e5139dcbfabaca4c4835b064c0b958
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-08-29 20:05:34 -0300

    Criando estruturas de dados

--- 2017/pc/aula3/exercicio.cpp
+++ 2017/pc/aula3/exercicio.cpp
@@ -9,6 +9,13 @@ typedef struct {
     float saldo;
 } Conta;
 
+float get_saldo( Conta &conta ) {
+    return conta.saldo;
+}
+
+void set_saldo( Conta &conta ) {
+    conta.saldo;
+}
 int main() {
     Conta conta;
 
