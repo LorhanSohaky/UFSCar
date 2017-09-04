fce4905257d4314377e2b3aa41a3571f4afd91b1
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-09-04 13:05:18 -0300

    Teste das funções

--- 2017/pc/aula4/exemplo2.cpp
+++ 2017/pc/aula4/exemplo2.cpp
@@ -55,7 +55,46 @@ class ArCondicionado {
 
 int main() {
     ArCondicionado ar( 1 );
-    ar.ligaDesliga();
-    ar.mostrar();
+
+    int opcao = 0;
+
+    do {
+        std::cout << "1- Liga/Desliga" << '\n';
+        std::cout << "2- Aumentar temperatura" << '\n';
+        std::cout << "3- Diminuir temperatura" << '\n';
+        std::cout << "4- Aumentar velocidade" << '\n';
+        std::cout << "5- Diminuir velocidade" << '\n';
+        std::cout << "6- Sair" << '\n';
+
+        std::cin >> opcao;
+
+        switch( opcao ) {
+            case 1:
+                ar.ligaDesliga();
+                break;
+            case 2:
+                ar.aumentaTemperatura();
+                break;
+            case 3:
+                ar.diminuiTemperatura();
+            case 4:
+                ar.aumentaVelocidade();
+                break;
+            case 5:
+                ar.diminuiVelocidade();
+                break;
+        }
+
+        for( int i = 0; i < 20; i++ ) {
+            std::cout << '\n';
+        }
+
+        if( opcao > 0 && opcao < 6 ) {
+            ar.mostrar();
+            std::cout << '\n';
+        }
+
+    } while( opcao != 6 );
+
     return 0;
 }
