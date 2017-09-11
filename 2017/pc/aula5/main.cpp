552b51386ae2affb26a71eb8db8a29a859d36f4f
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-09-11 19:55:24 -0300

    Correção do main

--- 2017/pc/aula5/main.cpp
+++ 2017/pc/aula5/main.cpp
@@ -1,10 +1,7 @@
 #include "SmartWatch.h"
-#include <string>
 
 int main() {
-    string nome = "Xiaomi";
-    string modelo = "Mi Band 2";
-    SmartWatch relogio( nome, modelo, 3, 10, 20, 90, 100, true );
+    SmartWatch relogio( "Xiaomi", "Mi Band 2", 3, 10, 20, 90, 100, true );
     relogio.printDados();
     return 0;
 }
