70b72b3641ea43078d8384015f71d36252f0ad5e
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-09-11 10:32:45 -0300

    Criação de Getters e Setters

--- 2017/pc/aula5/Horario.cpp
+++ 2017/pc/aula5/Horario.cpp
@@ -4,9 +4,20 @@
 class Horario {
   public:
     Horario( int hora, int minuto, int segundo );
+
     void setHorario( int hora, int minuto, int segundo );
     void printUniversal();
     void printStandard();
+
+    int getHora();
+    int getMinuto();
+    int getSegundo();
+
+    void setHora( int hora );
+    void setMinuto( int minuto );
+    void setSegundo( int segundo );
+
+    ~Horario();
 
   private:
     int hora, minuto, segundo;
@@ -16,22 +27,65 @@ Horario::Horario( int hora, int minuto, int segundo ) {
     std::cout << "Criação do Horario" << '\n';
 
     setHorario( hora, minuto, segundo );
+    printUniversal();
+}
+
+Horario::~Horario() {
+    std::cout << "Chamada do destrutor Horario" << '\n';
+    setHorario( 0, 0, 0 );
 }
 
 void Horario::setHorario( int hora, int minuto, int segundo ) {
-    this->hora = ( hora >= 0 && hora < 24 ) ? hora : 0;
-    this->minuto = ( minuto >= 0 && minuto < 60 ) ? minuto : 0;
-    this->segundo = ( segundo >= 0 && segundo < 60 ) ? segundo : 0;
+    setHora( hora );
+    setMinuto( minuto );
+    setSegundo( segundo );
 }
 
 void Horario::printUniversal() {
-    std::cout << std::setfill( '0' ) << std::setw( 2 ) << hora << " " << minuto << " " << segundo
+    std::cout << std::setfill( '0' ) << std::setw( 2 ) << hora << ":" << std::setfill( '0' )
+              << std::setw( 2 ) << minuto << ":" << std::setfill( '0' ) << std::setw( 2 ) << segundo
               << '\n';
 }
 
 void Horario::printStandard() {
     std::cout << ( ( hora >= 0 && hora < 12 ) ? hora : ( hora % 12 ) ) << ":" << minuto << ":"
               << segundo << " " << ( ( hora < 12 ) ? "AM" : "PM" ) << '\n';
+}
+
+int Horario::getHora() {
+    return hora;
+}
+
+int Horario::getMinuto() {
+    return minuto;
+}
+
+int Horario::getSegundo() {
+    return segundo;
+}
+
+void Horario::setHora( int hora ) {
+    if( hora >= 0 && hora < 24 ) {
+        this->hora = hora;
+    } else {
+        this->hora = 0;
+    }
+}
+
+void Horario::setMinuto( int minuto ) {
+    if( minuto >= 0 && minuto < 60 ) {
+        this->minuto = minuto;
+    } else {
+        this->minuto = 0;
+    }
+}
+
+void Horario::setSegundo( int segundo ) {
+    if( segundo >= 0 && segundo < 60 ) {
+        this->segundo = segundo;
+    } else {
+        this->segundo = 0;
+    }
 }
 
 int main() {
