1f377f961b0a5b3bc844f2b570bc44c8aa74b5fe
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-09-14 10:07:43 -0300

    Mais testes

--- 2017/pc/aula5/projeto/main.cpp
+++ 2017/pc/aula5/projeto/main.cpp
@@ -3,5 +3,14 @@
 int main() {
     SmartWatch relogio( "Xiaomi", "Mi Band 2", 3, 10, 20, 90, 100, true );
     relogio.printDados();
+
+    for( int i = 0; i < 10; i++ ) {
+        relogio.incrementaPassos();
+    }
+    relogio.printDados();
+
+    relogio.zerarPassos();
+    relogio.printDados();
+
     return 0;
 }
