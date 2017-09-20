0ce483d46da94cae1f4d400037adc2558bb0599b
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-09-19 22:51:50 -0300

    Criação da classe para chamar a tela e validação dos campos

---
+++ 2017/SECOMP/PythonQt/exemplo4/tela_form.py
@@ -0,0 +1,50 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from main_form import Ui_MainWindow
+from showDialog import showDialog
+
+
+class form1(QMainWindow):
+    def __init__(self, parent=None):
+        super(form1, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.connectSignal()
+
+    def connectSignal(self):
+        self.ui.btnOk.clicked.connect(self.some_action)
+
+    def validaCampos(self):
+        texto = ""
+
+        if not (self.ui.nome.text()):
+            texto += "Nome\n"
+        if not (self.ui.idade.text()):
+            texto += "Idade\n"
+        if not (self.ui.cpf.text()):
+            texto += "CPF\n"
+
+        return texto
+
+    def some_action(self):
+        campos = self.validaCampos()
+        if (campos == ""):
+            pass
+        else:
+            showDialog(
+                "Error",
+                "Todos os campos devem ser preenchidos. Campos a serem preenchidos:\n"
+                + campos)
+
+
+def main():
+    app = QApplication(sys.argv)
+    mw = form1()
+    mw.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
