2478ef89fdd82d7333cffa40d7c27e943e2c1a3a
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-09-19 23:23:36 -0300

    Criação do main da segunda tela

---
+++ 2017/SECOMP/PythonQt/exemplo4/tela_form2.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PyQt5.QtCore import Qt
+
+from form2 import Ui_MainWindow
+
+
+class form2(QMainWindow):
+    def __init__(self, nome, idade, cpf, parent=None):
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.setAttribute(Qt.Wa_DeleteOnClose)
+
+        self.ui.nome.setText(nome)
+        self.ui.idade.setText(cpf)
+        self.ui.cpf.setText(cpf)
+
+
+def main():
+    app = QApplication(sys.argv)
+    mw = form2()
+
+    mw.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
