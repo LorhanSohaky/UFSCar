f0512018352b69eba94c78bd78c97631c38962a4
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-19 18:42:03 -0200

    Correção do def

--- 2017/pc/tarefa2/Fracao.h
+++ 2017/pc/tarefa2/Fracao.h
@@ -3,7 +3,23 @@ TODO	Operações aritméticas, comparaçõ, entrada e saída
                 Operações entre frações e inteiros
 */
 
-#ifndef
+#ifndef FRACAO_H
 #define FRACAO_H
+
+class Fracao {
+  private:
+    int numerador;
+    int denominador;
+
+  public:
+    Fracao( const int numerador = 0, const int denominador = 1 );
+    ~Fracao( void );
+
+    void setNumerador( const int numerador );
+    int getNumerador( void ) const;
+
+    void setDenominador( const int denominador );
+    int getDenominador( void ) const;
+};
 
 #endif
