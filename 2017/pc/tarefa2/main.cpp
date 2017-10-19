20cd2cae8d01ae693c51253cc55bfcefe6e8574b
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-19 18:53:59 -0200

    Teste do operador de soma

--- 2017/pc/tarefa2/main.cpp
+++ 2017/pc/tarefa2/main.cpp
@@ -3,6 +3,11 @@
 
 int main( int argc, char const *argv[] ) {
     Fracao a( 1, 2 );
+    Fracao b( 1, 3 );
+
+    Fracao c = a + b;
+
+    std::cout << c.getNumerador() << "/" << c.getDenominador() << '\n';
 
     return 0;
 }
