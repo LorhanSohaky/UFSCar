3b1c7cfabef406c92b487634de473dd4cf8b8577
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-20 09:29:25 -0200

    Criação de funções de teste aritméticos

--- 2017/pc/tarefa2/main.cpp
+++ 2017/pc/tarefa2/main.cpp
@@ -1,16 +1,67 @@
 #include "Fracao.h"
 #include <iostream>
 
+void operacoesAritmeticas( void );
+
 int main( int argc, char const *argv[] ) {
-    Fracao a( 1, 2 );
-    Fracao b( 1, 3 );
-
-    Fracao c = a + b;
-
-    std::cout << c << '\n';
-
-    std::cin >> c;
-    std::cout << c << '\n';
+    operacoesAritmeticas();
 
     return 0;
+}
+
+void operacoesAritmeticas( void ) {
+    Fracao a( 1, 2 );
+    Fracao b( 1, 3 );
+    Fracao c( 5, 2 );
+    int d = 10;
+    int e = 0;
+
+    std::cout << a << "+" << b << "=" << a + b << '\n';
+    std::cout << a << "-" << b << "=" << a - b << '\n';
+    std::cout << a << "/" << b << "=" << a / b << '\n';
+    std::cout << a << "*" << b << "=" << a * b << '\n';
+
+    std::cout << '\n';
+
+    std::cout << b << "+" << a << "=" << b + a << '\n';
+    std::cout << b << "-" << a << "=" << b - a << '\n';
+    std::cout << b << "/" << a << "=" << b / a << '\n';
+    std::cout << b << "*" << a << "=" << b * a << '\n';
+
+    std::cout << '\n';
+
+    std::cout << a << "+" << c << "=" << a + c << '\n';
+    std::cout << a << "-" << c << "=" << a - c << '\n';
+    std::cout << a << "/" << c << "=" << a / c << '\n';
+    std::cout << a << "*" << c << "=" << a * c << '\n';
+
+    std::cout << '\n';
+
+    std::cout << a << "+" << d << "=" << a + d << '\n';
+    std::cout << a << "-" << d << "=" << a - d << '\n';
+    std::cout << a << "/" << d << "=" << a / d << '\n';
+    std::cout << a << "*" << d << "=" << a * d << '\n';
+
+    std::cout << '\n';
+
+    std::cout << d << "+" << a << "=" << d + a << '\n';
+    std::cout << d << "-" << a << "=" << d - a << '\n';
+    std::cout << d << "/" << a << "=" << d / a << '\n';
+    std::cout << d << "*" << a << "=" << d * a << '\n';
+
+    std::cout << '\n';
+
+    std::cout << a << "+" << d << "=" << a + e << '\n';
+    std::cout << a << "-" << d << "=" << a - e << '\n';
+    std::cout << a << "/" << d << "=" << a / e << '\n';
+    std::cout << a << "*" << d << "=" << a * e << '\n';
+
+    std::cout << '\n';
+
+    std::cout << d << "+" << a << "=" << e + a << '\n';
+    std::cout << d << "-" << a << "=" << e - a << '\n';
+    std::cout << d << "/" << a << "=" << e / a << '\n';
+    std::cout << d << "*" << a << "=" << e * a << '\n';
+
+    std::cout << '\n';
 }
