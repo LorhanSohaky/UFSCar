41dbe9b3df7b34be5e287ff298ddf38f0663f385
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-27 15:48:39 -0200

    Remoção da logo e mudança de local da ActionBar

--- 2017/Cati/React/apresentacao2/projeto1/react-notes/src/App.js
+++ 2017/Cati/React/apresentacao2/projeto1/react-notes/src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import ActionBar from './components/ActionBar'
 
 class App extends Component {
   render() {
     return (
 		<div id="root">
-
           <div id="wrapper">
-              <div class="row">
-                  <button class="btn"><i class="material-icons">add</i></button>
-                  <button class="btn btn-success"><i class="material-icons">archive</i></button>
-                  <button class="btn btn-danger"><i class="material-icons">delete</i></button>
-
-  		<div class="checkbox-container">
-  		    <input type="checkbox" />
-  		    <span>Exibir Arquivados</span>
-  		</div>
-              </div>
+		  	<ActionBar/>
               <div id="columns">
                   <div class="pin">
                       <button class="btn check-btn"><i class="material-icons">check_circle</i></button>
