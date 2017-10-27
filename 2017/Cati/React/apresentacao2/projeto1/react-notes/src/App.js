1c9cede4d86892c972f836fbe010392bd8f014f3
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-27 15:36:07 -0200

    Mudando estrutura

--- 2017/Cati/React/apresentacao2/projeto1/react-notes/src/App.js
+++ 2017/Cati/React/apresentacao2/projeto1/react-notes/src/App.js
@@ -5,14 +5,32 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+		<div id="root">
+
+          <div id="wrapper">
+              <div class="row">
+                  <button class="btn"><i class="material-icons">add</i></button>
+                  <button class="btn btn-success"><i class="material-icons">archive</i></button>
+                  <button class="btn btn-danger"><i class="material-icons">delete</i></button>
+
+  		<div class="checkbox-container">
+  		    <input type="checkbox" />
+  		    <span>Exibir Arquivados</span>
+  		</div>
+              </div>
+              <div id="columns">
+                  <div class="pin">
+                      <button class="btn check-btn"><i class="material-icons">check_circle</i></button>
+                      <p>Nota 0</p>
+                      <p>Conteúdo...</p>
+                      <div class="actions">
+                          <button class="btn action-btn"><i class="material-icons">archive</i></button>
+                          <button class="btn action-btn"><i class="material-icons">delete</i></button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
       </div>
     );
   }
