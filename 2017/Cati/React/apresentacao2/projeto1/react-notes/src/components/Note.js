474cc6866a25d5da323721930efb77b4dcbca0a4
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-27 17:30:09 -0200

    Atualização

--- 2017/Cati/React/apresentacao2/projeto1/react-notes/src/components/Note.js
+++ 2017/Cati/React/apresentacao2/projeto1/react-notes/src/components/Note.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-const Note = ({note}) => (
-	<div class="pin">
-		<button class="btn check-btn"><i class="material-icons">check_circle</i></button>
-		<p>{note.title}</p>
-		<p>{note.text}</p>
-		<div class="actions">
-			<button class="btn action-btn"><i class="material-icons">archive</i></button>
-			<button class="btn action-btn"><i class="material-icons">delete</i></button>
-		</div>
-	</div>
-)
+class Note extends React.Component{
 
+	constructor(props){
+		super(props);
+
+		this.state={
+			edit: null
+		}
+
+		this.handleClick=this.handleClick.bind(this);
+	}
+
+	handleClick(event){
+		const edit=event.target.id;
+
+		this.setState({
+			edit:edit
+		});
+	}
+
+	render(){
+		 return(
+			<div class="pin">
+				<button class="btn check-btn"><i class="material-icons">check_circle</i></button>
+				<p id="title">{this.props.note.title}</p>
+				<p id="text">{this.props.note.text}</p>
+				<div class="actions">
+					<button class="btn action-btn"><i class="material-icons">archive</i></button>
+					<button class="btn action-btn"><i class="material-icons">delete</i></button>
+				</div>
+			</div>
+		)
+	}
+}
 export default Note;
