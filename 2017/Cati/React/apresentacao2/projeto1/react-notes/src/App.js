61d95bdda9b6e1de88f4428d1f015abae4dd6bb6
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-10-27 15:54:29 -0200

    Separando board

--- 2017/Cati/React/apresentacao2/projeto1/react-notes/src/App.js
+++ 2017/Cati/React/apresentacao2/projeto1/react-notes/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import ActionBar from './components/ActionBar'
+import Board from './components/Board'
 
 class App extends Component {
   render() {
@@ -9,17 +10,7 @@ class App extends Component {
 		<div id="root">
           <div id="wrapper">
 		  	<ActionBar/>
-              <div id="columns">
-                  <div class="pin">
-                      <button class="btn check-btn"><i class="material-icons">check_circle</i></button>
-                      <p>Nota 0</p>
-                      <p>Conteúdo...</p>
-                      <div class="actions">
-                          <button class="btn action-btn"><i class="material-icons">archive</i></button>
-                          <button class="btn action-btn"><i class="material-icons">delete</i></button>
-                      </div>
-                  </div>
-              </div>
+              <Board/>
           </div>
 
       </div>
