ad677d27e1279780be447ae6b73cc94c6451dbeb
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-11-23 12:21:28 -0200

    Criação da janela utilizando a área de desenho

--- 2017/pc/tarefa3/MyWindow.cpp
+++ 2017/pc/tarefa3/MyWindow.cpp
@@ -3,6 +3,6 @@
 MyWindow::MyWindow( void ) {
     resize( 300, 200 );
     set_title( "Tarefa 3 de PC" );
-    // window.add( area );
-    // area.show();
+    add( area );
+    area.show();
 }
--- 2017/pc/tarefa3/MyWindow.h
+++ 2017/pc/tarefa3/MyWindow.h
@@ -1,10 +1,13 @@
 #ifndef MYWINDOW_H
 #define MYWINDOW_H
 
-//#include "MyArea.h"
+#include "MyArea.h"
 #include <gtkmm.h>
 
 class MyWindow : public Gtk::Window {
+  private:
+    MyArea area;
+
   public:
     MyWindow( void );
 };
