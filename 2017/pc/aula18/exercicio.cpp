693398c5d8e22d5b73e6d7154dcfa1b41fc55546
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-11-27 14:01:01 -0200

    Aprendendo template utilizando lista

---
+++ 2017/pc/aula18/exercicio.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T> class Lista {
+  public:
+    Lista( const int tamanho );
+    ~Lista();
+
+    int getTamanho() const;
+    bool setValue( const T &value, const int posicao );
+
+    T getValue( const int posicao ) const;
+
+    template <class U> friend ostream &operator<<( ostream &saida, const Lista<U> &lista );
+
+  private:
+    int tamanho;
+    T *lista;
+};
+
+template <class T> Lista<T>::Lista( const int tamanho ) {
+    if( tamanho > 0 ) {
+        this->tamanho = tamanho;
+    } else {
+        this->tamanho = 10;
+    }
+    lista = new T[this->tamanho];
+}
+
+template <class T> Lista<T>::~Lista() {
+    delete[] lista;
+}
+
+template <class T> bool Lista<T>::setValue( const T &value, const int posicao ) {
+    if( posicao >= 0 && posicao < tamanho ) {
+        lista[posicao] = value;
+        return true;
+    }
+    return false;
+}
+
+template <class T> T Lista<T>::getValue( const int posicao ) const {
+    if( posicao >= 0 && posicao < tamanho ) {
+        return lista[posicao];
+    }
+}
+
+template <class T> int Lista<T>::getTamanho() const {
+    return tamanho;
+}
+
+template <class T> ostream &operator<<( ostream &saida, Lista<T> &lista ) {
+    for( int i = 0; i < lista.getTamanho(); i++ ) {
+        saida << lista.getValue( i ) << "\t";
+    }
+    return saida;
+}
+
+int main( int argc, char const *argv[] ) {
+    Lista<int> l = ( 2 );
+    l.setValue( 100, 1 );
+    l.setValue( 200, 0 );
+    std::cout << l << '\n';
+    return 0;
+}
