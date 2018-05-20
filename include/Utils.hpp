67b8195470f25494a9023aa31bfb58c18e136407
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-05-20 18:23:34 -0300

    Adicionada estrutura que será passada para as telas

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,6 +1,18 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <SFML/Graphics.hpp>
+#include <memory>
+
 enum WINDOW_STATES { MENU = 1, JOGAR, CREDITOS, GANHOU, PERDEU, FECHAR };
+
+struct GameData {
+    sf::RenderWindow* window;
+    sf::Event event;
+    bool isAudioOn;
+    WINDOW_STATES estadoJanela;
+};
+
+typedef std::shared_ptr< GameData > GameRef;
 
 #endif
