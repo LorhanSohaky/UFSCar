2b74ad4913d0aa90276a5160d9e0be69d270cb44
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-05-23 04:01:42 -0300

    Screen tendo acesso ao nextScreen

--- src/model/Screen.cpp
+++ src/model/Screen.cpp
@@ -5,5 +5,6 @@ Screen::Screen( GameRef gameRef ) {
     this->window       = gameRef->window;
     this->event        = &gameRef->event;
     this->isAudioOn    = &gameRef->isAudioOn;
+    this->nextScreen   = &gameRef->nextScreen;
     this->inputManager = gameRef->inputManager;
 }
