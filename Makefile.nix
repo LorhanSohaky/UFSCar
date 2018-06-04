090fabc00a2c240b46100d8c2128c97f18ce1e0a
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-03 22:07:10 -0300

    Agrupando os objetos em um arquivo

--- Makefile.nix
+++ Makefile.nix
@@ -1,38 +1,57 @@
 CXX = g++
 
+OUT = Default
+
+OBJDIR = obj
+BINDIR = bin
+
 IDIR = -I./include
+SOURCES_PATH = *.cpp src/model/*.cpp src/viewer/*.cpp src/controller/*.cpp
+
 CXXFLAGS = -Wall -std=c++14 -O3 $(IDIR)
 LIBS = -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system
 
-SOURCES_PATH = *.cpp src/model/*.cpp src/viewer/*.cpp src/controller/*.cpp
 
 define listar_os_sources
 	$(foreach source,$1,$(wildcard $(source)))
+endef
+
+define listar_as_pastas
+	$(foreach source,$1,$(dir $(source)))
 endef
 
 define listar_os_objetos
 	$(foreach source, $1,$(patsubst %.cpp,%.o,$(source)))
 endef
 
+define adicionar_pasta_obj
+	$(foreach source, $1,$(addprefix $(OBJDIR)/,$(source)))
+endef
+
 
 SOURCES := $(call listar_os_sources,$(SOURCES_PATH))
-OBJS := $(call listar_os_objetos,$(SOURCES))
 
-OUT = bin/Default
+OBJS := $(call listar_os_objetos,$(SOURCES))
+OBJS := $(call adicionar_pasta_obj,$(OBJS))
+
+DIRLIST := $(call listar_as_pastas,$(SOURCES_PATH))
+DIRLIST := $(call adicionar_pasta_obj,$(DIRLIST))
+
 
 install: makeDir compile
 
 makeDir:
-	@mkdir bin -p
+	@mkdir -p $(BINDIR)
+	@mkdir -p $(DIRLIST)
 
-%.o: $.cpp
-	$(CXX) -c -o  $@ $< $(CXXFLAGS)
+$(OBJDIR)/%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 compile: $(OBJS)
-	$(CXX) -o $(OUT) $^ $(CXXFLAGS) $(LIBS)
+	$(CXX) -o $(BINDIR)/$(OUT) $^ $(CXXFLAGS) $(LIBS)
 
 run: install
-	@cd bin && ./Default
+	@cd $(BINDIR) && ./$(OUT)
 
 clean:
-	@rm $(OBJS)
+	@rm -rdf $(OBJDIR)
