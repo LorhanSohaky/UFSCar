52c18fe69287b0cbcad3e8fc2d85c05726ace6c2
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-11 15:03:23 -0300

    Adicionado botão de créditos e correção do alinhamento

--- src/viewer/ScreenMenu.cpp
+++ src/viewer/ScreenMenu.cpp
@@ -8,7 +8,11 @@ ScreenMenu::ScreenMenu( GameRef& gameRef )
     loadAssets();
 
     playButton.setPosition( WINDOW_WIDTH - playButton.getGlobalBounds().width,
-                            ( WINDOW_HEIGHT - playButton.getGlobalBounds().height ) / 2 );
+                            ( WINDOW_HEIGHT - playButton.getGlobalBounds().height ) / 2 - 30 );
+
+    creditsButton.setPosition( WINDOW_WIDTH - creditsButton.getGlobalBounds().width,
+                               playButton.getGlobalBounds().top +
+                                   playButton.getGlobalBounds().height );
 }
 
 void ScreenMenu::loadAssets() {
@@ -17,12 +21,16 @@ void ScreenMenu::loadAssets() {
 
     TextureManager::add( "playButton", "play.png", true );
     playButton.setTexture( TextureManager::get( "playButton" ) );
+
+    TextureManager::add( "creditsButton", "credits.png", true );
+    creditsButton.setTexture( TextureManager::get( "creditsButton" ) );
 }
 void ScreenMenu::draw() {
     window->clear();
 
     window->draw( background );
     window->draw( playButton );
+    window->draw( creditsButton );
 
     window->display();
 }
