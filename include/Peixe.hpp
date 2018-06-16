33a12a2232cb2420b5b132c5ae4c77042625d5e1
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-16 13:02:33 -0300

    Criada a classe Peixe

---
+++ include/Peixe.hpp
@@ -0,0 +1,12 @@
+#ifndef PEIXE_HPP
+#define PEIXE_HPP
+
+#include "Food.hpp"
+#include <SFML/Graphics.hpp>
+
+class Peixe : public Food {
+  public:
+    Peixe();
+};
+
+#endif
---
+++ src/model/Peixe.cpp
@@ -0,0 +1,5 @@
+#include "../../include/Peixe.hpp"
+
+Peixe::Peixe()
+    : Food( "peixe", "foods/comida1.png", false ) {
+}
