e3390a992b9bf99fe427fbfcabec752888e2918f
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-16 12:35:22 -0300

    Criada a classe Picles

---
+++ include/Picles.hpp
@@ -0,0 +1,12 @@
+#ifndef PICLES_HPP
+#define PICLES_HPP
+
+#include "Food.hpp"
+#include <SFML/Graphics.hpp>
+
+class Picles : public Food {
+  public:
+    Picles();
+};
+
+#endif
---
+++ src/model/Picles.cpp
@@ -0,0 +1,5 @@
+#include "../../include/Picles.hpp"
+
+Picles::Picles()
+    : Food( "picles", "foods/picles.png", true ) {
+}
