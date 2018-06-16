2803d1bf42bd2990e5c4731c08f1a3c5bcb89d2c
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-16 01:45:18 -0300

    Corrigido

--- src/model/Food.cpp
+++ src/model/Food.cpp
@@ -1,11 +1,11 @@
 #include "../../include/Food.hpp"
 #include "../../include/TextureManager.hpp"
 
-Food::Food( const sf::Texture& texture, const bool& comidaCerta ) {
-    sprite.setTexture( texture );
+Food::Food( const sf::Texture& texture, const bool& comidaCerta )
+    : Sprite( texture ) {
     this->comidaCerta = comidaCerta;
 }
 
 void Food::draw( sf::RenderTarget& target, sf::RenderStates states ) const {
-    target.draw( sprite, states );
+    target.draw( ( sf::Sprite ) * this, states );
 }
