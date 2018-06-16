890151065a9beac7a3d5e7eb6d36f3410f061e42
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-16 12:28:46 -0300

    Criada classe pão inferior

---
+++ include/PaoInferior.hpp
@@ -0,0 +1,12 @@
+#ifndef PAO_INFERIOR_HPP
+#define PAO_INFERIOR_HPP
+
+#include "Food.hpp"
+#include <SFML/Graphics.hpp>
+
+class PaoInferior : public Food {
+  public:
+    PaoInferior();
+};
+
+#endif
