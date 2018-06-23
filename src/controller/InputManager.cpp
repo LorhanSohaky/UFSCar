e7582d4d5b161a47bf5c2bf1f0256f345b5ac002
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-23 18:04:44 -0300

    Correção dos eventos de teclado

--- src/controller/InputManager.cpp
+++ src/controller/InputManager.cpp
@@ -6,12 +6,12 @@ InputManager::InputManager( sf::Event* event, sf::RenderWindow* window ) {
     this->window = window;
 }
 
-bool InputManager::keyPressed( const sf::Event::KeyEvent key ) {
-    return event->type == sf::Event::KeyPressed && event->key.code == key.code;
+bool InputManager::keyPressed( const sf::Keyboard::Key& key ) {
+    return event->type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed( key );
 }
 
-bool InputManager::keyPressed( const std::vector< sf::Event::KeyEvent >& keys ) {
-    for( sf::Event::KeyEvent key : keys ) {
+bool InputManager::keyPressed( const std::vector< sf::Keyboard::Key >& keys ) {
+    for( sf::Keyboard::Key key : keys ) {
         if( keyPressed( key ) ) {
             return true;
         }
@@ -20,12 +20,12 @@ bool InputManager::keyPressed( const std::vector< sf::Event::KeyEvent >& keys ) 
     return false;
 }
 
-bool InputManager::keyReleased( const sf::Event::KeyEvent key ) {
-    return event->type == sf::Event::KeyReleased && event->key.code == key.code;
+bool InputManager::keyReleased( const sf::Keyboard::Key& key ) {
+    return event->type == sf::Event::KeyReleased && !sf::Keyboard::isKeyPressed( key );
 }
 
-bool InputManager::keyReleased( const std::vector< sf::Event::KeyEvent >& keys ) {
-    for( sf::Event::KeyEvent key : keys ) {
+bool InputManager::keyReleased( const std::vector< sf::Keyboard::Key >& keys ) {
+    for( sf::Keyboard::Key key : keys ) {
         if( keyReleased( key ) ) {
             return true;
         }
