b61607f223db0b362fea4cfa5bc25c922fd1c51d
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-06-23 22:54:53 -0300

    Adicionada função para pegar o alias

--- include/Food.hpp
+++ include/Food.hpp
@@ -6,12 +6,15 @@
 class Food : public sf::Sprite {
   public:
     explicit Food( const std::string alias, const std::string filePath, const bool& comidaCerta );
+
     bool getComidaCerta() const;
+    std::string getAlias() const;
 
   protected:
     void draw( sf::RenderTarget& target, sf::RenderStates states ) const;
 
     bool comidaCerta;
+    std::string alias;
     sf::Sprite sprite;
 };
 
