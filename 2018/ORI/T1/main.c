42479f97e596a9ef8267549fd4b25576b287bc7a
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-08-25 14:35:46 -0300

    Adicionada macros Adicionada estrutura little-endian Função para escrever em bloco Função para converter para little-endian

--- 2018/ORI/T1/main.c
+++ 2018/ORI/T1/main.c
@@ -10,7 +10,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_BYTES 510 // 512 - 2 bytes little-endian
+#define BLOCK_SIZE 512
+#define MAX_BYTES ( 512 - 2 ) // 512 - 2 bytes little-endian
+
 #define ADITIONAL_BYTES 5
 
 typedef struct {
@@ -20,14 +22,20 @@ typedef struct {
     char ano[ 5 ];
 } Registro;
 
-int main( int argc, char const *argv[] ) {
-    char nomeArquivo[ 200 ];
-    unsigned int quantidadeRegistros = 0;
-    scanf( "%s%d", nomeArquivo, &quantidadeRegistros );
+typedef struct {
+    char number[ 2 ];
+} LittleEndian;
 
+void escreverBloco( FILE *arquivo, char *bloco, const unsigned short int quantidadeBytes );
+LittleEndian toLittleEndian( const unsigned short int numero );
+
+int main( int argc, char const *argv[] ) {
+    char nomeArquivo[ 21 ];
+    unsigned int quantidadeRegistros = 0;
+
+    scanf( "%s%d", nomeArquivo, &quantidadeRegistros );
     strcat( nomeArquivo, ".dat" );
 
-    Registro registro;
     FILE *arquivo = fopen( nomeArquivo, "w" );
 
     if( !arquivo ) {
@@ -35,21 +43,27 @@ int main( int argc, char const *argv[] ) {
         exit( EXIT_FAILURE );
     }
 
+    char bloco[ 512 ];
+    Registro registro;
+
     unsigned int quantidadeBytes = 0;
     for( unsigned int i = 0; i < quantidadeRegistros; i++ ) {
-        scanf( " %s", registro.ra );
-        scanf( " %[^\n]", registro.nome );
-        scanf( " %s", registro.curso );
-        scanf( " %s", registro.ano );
+        scanf( "%s ", registro.ra );
+        scanf( "%[^\n] ", registro.nome );
+        scanf( "%s ", registro.curso );
+        scanf( "%s ", registro.ano );
 
         unsigned int tamanhoRegistro = strlen( registro.ra ) + strlen( registro.nome ) +
                                        strlen( registro.curso ) + strlen( registro.ano );
         tamanhoRegistro *= sizeof( char );
         tamanhoRegistro += ADITIONAL_BYTES;
 
-        quantidadeBytes += tamanhoRegistro;
+        if( quantidadeBytes + tamanhoRegistro > MAX_BYTES ) {
+            escreverBloco( arquivo, bloco, quantidadeBytes );
+            quantidadeBytes = 0;
+        }
 
-        fprintf( arquivo,
+        sprintf( &bloco[ quantidadeBytes ],
                  "%s%c%s%c%s%c%s%c%c",
                  registro.ra,
                  0x0D,
@@ -60,9 +74,29 @@ int main( int argc, char const *argv[] ) {
                  registro.ano,
                  0x0D,
                  0x0A );
+        quantidadeBytes += tamanhoRegistro;
     }
+
+    escreverBloco( arquivo, bloco, quantidadeBytes );
 
     fclose( arquivo );
 
     return 0;
+}
+
+void escreverBloco( FILE *arquivo, char *bloco, const unsigned short int quantidadeBytes ) {
+    LittleEndian little = toLittleEndian( quantidadeBytes );
+    sprintf( &bloco[ MAX_BYTES ], "%c%c", little.number[ 0 ], little.number[ 1 ] );
+    fwrite( bloco, sizeof( char ) * BLOCK_SIZE, 1, arquivo );
+}
+
+LittleEndian toLittleEndian( const unsigned short int numero ) {
+    unsigned short int primeiroByte = ( numero & 0xFF00 ) >> 8; // 0xFF00 = 11111111 00000000
+    unsigned short int segundoByte  = ( numero & 0xFF );        // 0xFF = 00000000 11111111
+
+    LittleEndian little;
+    little.number[ 0 ] = segundoByte;
+    little.number[ 1 ] = primeiroByte;
+
+    return little;
 }
