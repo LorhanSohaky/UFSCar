0783a925c1a40156ee7c5566d60aeeb592a2863d
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-11-16 00:12:20 -0200

    Abrindo arquivos e contando a quantidade de registros

--- 2018/ORI/T2/main.c
+++ 2018/ORI/T2/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,11 @@
 int ordenar( const char *nomeBase );
 bool existeArquivo( const char *nomeArquivo );
 int ordenar_por_intercalacao_balanceada( const char *nomeBase, int quantidadeArquivos );
+int abrirArquivos( FILE **arquivosEntrada,
+                   FILE **arquivosSaida,
+                   const char *nomeBase,
+                   char *nomeArquivo,
+                   int quantidadeArquivos );
 void selectionSort( int *vetor, int tamanho );
 
 int main( int argc, char const *argv[] ) {
@@ -16,8 +22,7 @@ int main( int argc, char const *argv[] ) {
         fprintf( stderr, "Nome base do arquivo nao especificado!\n" );
         return EXIT_FAILURE;
     }
-    ordenar( argv[ 1 ] );
-    return EXIT_SUCCESS;
+    return ordenar( argv[ 1 ] );
 }
 
 int ordenar( const char *nomeBase ) {
@@ -45,6 +50,7 @@ int ordenar( const char *nomeBase ) {
     }
 
     ordenar_por_intercalacao_balanceada( nomeBase, quantidadeArquivos );
+    return EXIT_SUCCESS;
 }
 
 bool existeArquivo( const char *nomeArquivo ) {
@@ -59,23 +65,42 @@ bool existeArquivo( const char *nomeArquivo ) {
 }
 
 int ordenar_por_intercalacao_balanceada( const char *nomeBase, int quantidadeArquivos ) {
-    FILE **arquivos = malloc( quantidadeArquivos * sizeof( FILE * ) );
-    if( arquivos == NULL ) {
-        fprintf( stderr, "Problema com alocacao dinamica! Possivel falta de memoria\n" );
-        return EXIT_FAILURE;
-    }
-
-    char *nomeArquivo = malloc( strlen( nomeBase + MAX_ARQUIVOS ) * sizeof( char ) );
-    if( nomeArquivo == NULL ) {
-        free( arquivos );
-        fprintf( stderr, "Problema com alocacao dinamica! Possivel falta de memoria\n" );
-        return EXIT_FAILURE;
-    }
+    FILE **arquivosEntrada = malloc( quantidadeArquivos * sizeof( FILE * ) );
+    FILE **arquivosSaida   = malloc( quantidadeArquivos * sizeof( FILE * ) );
+    char *nomeArquivo      = malloc( strlen( nomeBase + MAX_ARQUIVOS ) * sizeof( char ) );
+    // TODO: Verificar erros de alocação
 
     for( int i = 0; i < quantidadeArquivos; i++ ) {
         sprintf( nomeArquivo, "%s%d", nomeBase, i );
-        arquivos[ i ] = fopen( nomeArquivo, "rb" );
+        arquivosEntrada[ i ] = fopen( nomeArquivo, "rb" );
+        // TODO: Verificar erros de alocação e liberar memória
     }
+
+    for( int i = 0; i < quantidadeArquivos; i++ ) {
+        sprintf( nomeArquivo, "%s%d", nomeBase, i + quantidadeArquivos );
+        arquivosSaida[ i ] = fopen( nomeArquivo, "wb" );
+        // TODO: Verificar erros de alocação e liberar memória
+    }
+
+    float totalRegistros = 0;
+
+    for( int i = 0; i < quantidadeArquivos; i++ ) {
+        fseek( arquivosEntrada[ i ], 0, SEEK_END );
+        totalRegistros += ftell( arquivosEntrada[ 0 ] ) / sizeof( int );
+        rewind( arquivosEntrada[ 0 ] );
+    }
+
+    for( int i = 0;
+         i < ceil( log10( totalRegistros / quantidadeArquivos ) / log10( quantidadeArquivos * 2 ) );
+         i++ ) {
+    }
+
+    // TODO ordenar
+
+    // TODO: fechar arquivos
+    // TODO: leberar alocação
+
+    return EXIT_SUCCESS;
 }
 
 void selectionSort( int *vetor, int tamanho ) {
