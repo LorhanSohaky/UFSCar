63acbd979048580e513139c84b23343b680ae19c
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-11-23 15:17:30 -0200

    Arrumando para fazer somente a intercalação dos arquivos

--- 2018/ORI/T2/main.c
+++ 2018/ORI/T2/main.c
@@ -8,11 +8,14 @@
 #define MAX_REGISTROS_POR_ARQUIVO 5 // Número máximo de registros lidos de cada arquivo
 
 int ordenar( const char *nomeBase );
+int contarArquivos( const char *nomeBase );
 bool existeArquivo( const char *nomeArquivo );
-int ordenar_por_intercalacao_balanceada( const char *nomeBase, int quantidadeArquivos );
-int contarRegistros( FILE **arquivosEntrada, int quantidadeArquivos );
-
-void selectionSort( int vetor[ MAX_REGISTROS_POR_ARQUIVO ], int tamanho );
+int intercalar( const char *nomeBase, int quantidadeArquivos );
+bool leuTodosOsArquivosPorCompleto( FILE *arquivosEntrada[ MAX_ARQUIVOS ], int quantidadeArquivos );
+int pegaMenor( const int buffer[][ MAX_REGISTROS_POR_ARQUIVO ],
+               const int quantidadeArquivos,
+               const int *indices,
+               const int *lidoAtual );
 
 int main( int argc, char const *argv[] ) {
     if( argc != 2 ) {
@@ -31,6 +34,25 @@ int ordenar( const char *nomeBase ) {
         return EXIT_FAILURE;
     }
 
+    quantidadeArquivos = contarArquivos( nomeBase );
+
+    if( quantidadeArquivos == 0 ) {
+        fprintf( stderr, "Nao existe outros arquivos a patir do arquivo base especificado!\n" );
+        return EXIT_FAILURE;
+    }
+
+    return intercalar( nomeBase, quantidadeArquivos );
+}
+
+int contarArquivos( const char *nomeBase ) {
+    int quantidadeArquivos = 0;
+    char *nomeArquivo      = malloc( strlen( nomeBase + MAX_ARQUIVOS ) * sizeof( char ) );
+
+    if( nomeArquivo == NULL ) {
+        fprintf( stderr, "Problema com alocacao dinamica! Possivel falta de memoria\n" );
+        return EXIT_FAILURE;
+    }
+
     for( int i = 0; i < MAX_ARQUIVOS; i++ ) {
         sprintf( nomeArquivo, "%s%d", nomeBase, i );
 
@@ -41,13 +63,7 @@ int ordenar( const char *nomeBase ) {
 
     free( nomeArquivo );
 
-    if( quantidadeArquivos == 0 ) {
-        fprintf( stderr, "Nao existe outros arquivos a patir do arquivo base especificado!\n" );
-        return EXIT_FAILURE;
-    }
-
-    ordenar_por_intercalacao_balanceada( nomeBase, quantidadeArquivos );
-    return EXIT_SUCCESS;
+    return quantidadeArquivos;
 }
 
 bool existeArquivo( const char *nomeArquivo ) {
@@ -61,103 +77,100 @@ bool existeArquivo( const char *nomeArquivo ) {
     return result;
 }
 
-int ordenar_por_intercalacao_balanceada( const char *nomeBase, int quantidadeArquivos ) {
+int intercalar( const char *nomeBase, int quantidadeArquivos ) {
     FILE *arquivosEntrada[ MAX_ARQUIVOS ];
-    FILE *arquivosSaida[ MAX_ARQUIVOS ];
+    FILE *arquivoSaida;
     int buffer[ MAX_ARQUIVOS ][ MAX_REGISTROS_POR_ARQUIVO ];
-    char *nomeArquivo = malloc( strlen( nomeBase + MAX_ARQUIVOS ) * sizeof( char ) );
-    // TODO: Verificar erros de alocação
 
-    // ABRINDO ARQUIVOS
+    char *nomeArquivo = malloc( strlen( nomeBase + MAX_ARQUIVOS ) * sizeof( char ) );
+    if( nomeArquivo == NULL ) {
+        fprintf( stderr, "Problema com alocacao dinamica! Possivel falta de memoria\n" );
+        return EXIT_FAILURE;
+    }
+
     for( int i = 0; i < quantidadeArquivos; i++ ) {
         sprintf( nomeArquivo, "%s%d", nomeBase, i );
-        arquivosEntrada[ i ] = fopen( nomeArquivo, "rb+" );
+        arquivosEntrada[ i ] = fopen( nomeArquivo, "rb" );
+        if( arquivosEntrada[ i ] == NULL ) {
+            fprintf( stderr, "Problema na abertura do arquivo %s\n", nomeArquivo );
+            // TODO: Verificar erros de alocação e liberar memória
+        }
+    }
+
+    arquivoSaida = fopen( nomeBase, "wb" );
+    if( arquivoSaida == NULL ) {
+        fprintf( stderr, "Problema na abertura do arquivo %s\n", nomeBase );
         // TODO: Verificar erros de alocação e liberar memória
     }
 
-    for( int i = 0; i < quantidadeArquivos; i++ ) {
-        sprintf( nomeArquivo, "%s%d", nomeBase, i + quantidadeArquivos );
-        arquivosSaida[ i ] = fopen( nomeArquivo, "wb+" );
-        // TODO: Verificar erros de alocação e liberar memória
-    }
-
-    float totalRegistros          = contarRegistros( arquivosEntrada, quantidadeArquivos );
-    int lidoAtual[ MAX_ARQUIVOS ] = {0};
-
-    int totalPassos =
-        4; // TODO: mudar para
-           // teto(log10(totalRegistros/(quantidadeArquivos*MAX_REGISTROS_POR_ARQUIVO))/log10(2*quantidadeArquivos))
-    int lidoTotal = 0;
-
-    // Pegando os registros dos arquivos de entrada e ordenando
-    for( int i = 0; i < quantidadeArquivos; i++ ) {
-        lidoAtual[ i ] =
-            fread( &buffer[ i ], MAX_REGISTROS_POR_ARQUIVO, sizeof( int ), arquivosEntrada[ i ] );
-        lidoTotal += lidoAtual[ i ];
-        selectionSort( buffer[ i ], lidoAtual[ i ] );
-    }
-
-    int indices[ MAX_ARQUIVOS ] = {0};
-
-    while( lidoTotal > 0 ) {
-        int min = -1;
-
-        // inicializa min
-        for( int i = 0; i < quantidadeArquivos; i++ ) {
-            if( lidoAtual[ i ] > 0 ) {
-                min = i;
-                break;
-            }
-        }
-
-        if( min == -1 ) {
-            break;
-        }
+    while( !leuTodosOsArquivosPorCompleto( arquivosEntrada, quantidadeArquivos ) ) {
+        int lidoAtual[ MAX_ARQUIVOS ] = {0};
 
         for( int i = 0; i < quantidadeArquivos; i++ ) {
-            if( buffer[ min ][ indices[ min ] ] > buffer[ i ][ indices[ i ] ] ) {
-                min = i;
-            }
+            lidoAtual[ i ] = fread(
+                buffer[ i ], sizeof( int ), MAX_REGISTROS_POR_ARQUIVO, arquivosEntrada[ i ] );
         }
 
-        fwrite( &buffer[ min ][ indices[ min ] ], 1, sizeof( int ), arquivosSaida[ 0 ] );
-        printf( "min:%d %d\n", min, buffer[ min ][ indices[ min ] ] );
-        indices[ min ]++;
-        lidoAtual[ min ]--;
-        lidoTotal--;
+        int indices[ MAX_ARQUIVOS ] = {0};
+
+        int min = pegaMenor( buffer, quantidadeArquivos, indices, lidoAtual );
+        while( min != -1 ) {
+            fwrite( &buffer[ min ][ indices[ min ] ], sizeof( int ), 1, arquivoSaida );
+            indices[ min ]++;
+
+            if( indices[ min ] ==
+                lidoAtual[ min ] ) { // Para cobrir o caso de o último elemento do buffer 1 ser
+                                     // maior que o primeiro elemento da próxima leitura do buffer 2
+                lidoAtual[ min ] = fread( buffer[ min ],
+                                          sizeof( int ),
+                                          MAX_REGISTROS_POR_ARQUIVO,
+                                          arquivosEntrada[ min ] );
+                indices[ min ]   = 0;
+            }
+            min = pegaMenor( buffer, quantidadeArquivos, indices, lidoAtual );
+        }
     }
 
-    // TODO ordenar
-
-    // TODO: fechar arquivos
-    // TODO: leberar alocação
+    for( int i = 0; i < quantidadeArquivos; i++ ) {
+        fclose( arquivosEntrada[ i ] );
+    }
+    fclose( arquivoSaida );
+    free( nomeArquivo );
 
     return EXIT_SUCCESS;
 }
 
-int contarRegistros( FILE **arquivosEntrada, int quantidadeArquivos ) {
-    int totalRegistros = 0;
+bool leuTodosOsArquivosPorCompleto( FILE *arquivosEntrada[ MAX_ARQUIVOS ],
+                                    int quantidadeArquivos ) {
     for( int i = 0; i < quantidadeArquivos; i++ ) {
-        fseek( arquivosEntrada[ i ], 0, SEEK_END );
-        totalRegistros += ftell( arquivosEntrada[ i ] ) / sizeof( int );
-        rewind( arquivosEntrada[ i ] );
+        if( !feof( arquivosEntrada[ i ] ) ) {
+            return false;
+        }
     }
-    return totalRegistros;
+    return true;
 }
 
-void selectionSort( int vetor[ MAX_REGISTROS_POR_ARQUIVO ], int tamanho ) {
-    int i, j, min, aux;
-    for( i = 0; i < tamanho - 1; i++ ) {
-        min = i;
-        for( j = i + 1; j < tamanho; j++ ) {
-            if( vetor[ j ] < vetor[ min ] ) {
-                min = j;
-            }
-        }
-        if( vetor[ i ] != vetor[ min ] ) {
-            aux          = vetor[ i ];
-            vetor[ i ]   = vetor[ min ];
-            vetor[ min ] = aux;
+int pegaMenor( const int buffer[][ MAX_REGISTROS_POR_ARQUIVO ],
+               const int quantidadeArquivos,
+               const int *indices,
+               const int *lidoAtual ) {
+    int min = -1;
+
+    for( int i = 0; i < quantidadeArquivos; i++ ) {
+        if( lidoAtual[ i ] > 0 && indices[ i ] < lidoAtual[ i ] ) {
+            min = i;
+            break;
         }
     }
+
+    if( min != -1 ) {
+        for( int i = 0; i < quantidadeArquivos; i++ ) {
+            if( buffer[ min ][ indices[ min ] ] > buffer[ i ][ indices[ i ] ] &&
+                indices[ i ] < lidoAtual[ i ] ) {
+                min = i;
+            }
+        }
+    }
+
+    return min;
 }
