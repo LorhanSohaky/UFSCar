9a737820310afa8085948f3c87c5509d4a375577
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-05-08 09:17:34 -0300

    Correção do normalize que estava removendo as listas vazias. Criação do conta_atomos para contar os átomos de uma lista e o minus para fazer a diferença de conjuntos.

--- T1/trabalho.pl
+++ T1/trabalho.pl
@@ -3,7 +3,10 @@
 %    normalizar(L1,Resultado) -> resultando numa lista sem sublistas e sem variáveis
 % Pegando o resultado das execuções anterioes:
 %    interseccao(L1,L2, LResultado) -> isso irá remover os itens que estão em ambas as listas, mas se um item aparecer mais de uma vez em somente uma delas, ele irá aparecer no resultado
-%    removeRepeticao(Lista, Resultante) -> a partir de LResultado criar uma lista sem repetição
+%    minus(L1,LResultado,R1) -> R1 será tudo de L1 que não está em L2
+%    minus(L2,LResultado,R2) -> R2 será tudo de L2 que não está em L1
+%    append(R1,R2,R3) -> R3 será tudo que não está na intersecção de L1 com L2
+%    removeRepeticao(Lista, Resultante) -> a partir de R3 criar uma lista sem repetição
 %    contar(Resultante,LResultado, Resultado) -> contar a quantidade de vezes em que um item apareceu na lista e resultar uma lista em pares [[item,quantidade],...]
 concat([], L, L) :- !.
 concat([X|Y], L, [X|Z]) :-
@@ -12,6 +15,7 @@ concat([X|Y], L, [X|Z]) :-
 normalize([], []) :- !.
 normalize([X|Y], Z) :-
     is_list(X),
+    compound(X),
     concat(X, Y, L),
     normalize(L, Z), !.
 normalize([X|Y], [X|Z]) :-
@@ -50,3 +54,20 @@ countList([], L2, []) :-
 countList([X|Y], L2, [[X, Z]|W]) :-
     countItem(X, L2, Z),
     countList(Y, L2, W), !.
+
+minus([], _, []) :- !.
+minus([X|Y], Z, [X|W]) :-
+    not(member(X, Z)),
+    minus(Y, Z, W), !.
+minus([_|Y], Z, W) :-
+    minus(Y, Z, W).
+
+conta_atomos(L1, L2, Lout) :-
+    normalize(L1, M1),
+    normalize(L2, M2),
+    intersection(M1, M2, N2),
+    minus(M1, N2, O1),
+    minus(M2, N2, O2),
+    append(O1, O2, O3),
+    removeRepeated(O3, P1),
+    countList(P1, O3, Lout).
