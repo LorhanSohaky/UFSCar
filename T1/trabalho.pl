8019898c1bf3ede26078740f2d76fb1cbbb98158
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-05-07 23:11:55 -0300

    Normalize removendo variável também

--- T1/trabalho.pl
+++ T1/trabalho.pl
@@ -1,7 +1,6 @@
 % A ideia do algoritmo para resolver este problema é:
 % Para cada uma das listas executar:
-%    normalizar(L1,Resultado) -> resultando numa lista sem sublistas
-%    remover(Lista,Resultado) -> resulta em lista sem variáveis
+%    normalizar(L1,Resultado) -> resultando numa lista sem sublistas e sem variáveis
 % Pegando o resultado das execuções anterioes:
 %    interseccao(L1,L2, LResultado) -> isso irá remover os itens que estão em ambas as listas, mas se um item aparecer mais de uma vez em somente uma delas, ele irá aparecer no resultado
 %    removeRepeticao(Lista, Resultante) -> a partir de LResultado criar uma lista sem repetição
@@ -16,15 +15,10 @@ normalize([X|Y], Z) :-
     concat(X, Y, L),
     normalize(L, Z), !.
 normalize([X|Y], [X|Z]) :-
-    normalize(Y, Z).
-
-removeVar([], []) :- !.
-removeVar([X|Y], [X|Z]) :-
     not(var(X)),
-    removeVar(Y, Z), !.
-removeVar([_|Y], Z) :-
-    removeVar(Y, Z).
-
+    normalize(Y, Z), !.
+normalize([_|Y], Z) :-
+    normalize(Y, Z).
 
 in(X, [X|_]) :- !.
 in(X, [_|Y]) :-
