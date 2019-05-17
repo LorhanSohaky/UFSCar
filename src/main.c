0788e323a063622e0590453e12495c6c3d50f1c2
LorhanSohaky <lorhansohaky@gmail.com>
2019-05-17 18:32:25 -0300

    Arrumando formatação

--- src/main.c
+++ src/main.c
@@ -17,53 +17,55 @@ void join_threads();
 void *doSomething( void *args );
 
 int main() {
-    create_threads();
-    join_threads();
-
-    return 0;
+	create_threads();
+	join_threads();
+	return 0;
 }
 
 void create_threads() {
-    int result;
-    long int i;
-    for( i = 0; i < MAX_HACKERS; i++ ) {
-	if( ( result = pthread_create( &hackers[ i ], NULL, doSomething, (void *)i ) ) ) {
-	    printf( "Erro criando thread do hacker[%ld]\n", i );
-	    exit( 0 );
+	int result;
+	long int i;
+	for( i = 0; i < MAX_HACKERS; i++ ) {
+		if( ( result = pthread_create( &hackers[ i ], NULL, doSomething, (void *)i ) ) ) {
+			printf( "Erro criando thread do hacker[%ld]\n", i );
+			exit( 0 );
+		}
 	}
-    }
 
-    for( i = 0; i < MAX_SERFS; i++ ) {
-	if( ( result = pthread_create( &serfs[ i ], NULL, doSomething, (void *)i ) ) ) {
-	    printf( "Erro criando thread do serf[%ld]\n", i );
-	    exit( 0 );
+	for( i = 0; i < MAX_SERFS; i++ ) {
+		if( ( result = pthread_create( &serfs[ i ], NULL, doSomething, (void *)i ) ) ) {
+			printf( "Erro criando thread do serf[%ld]\n", i );
+			exit( 0 );
+		}
 	}
-    }
 }
 
 void join_threads() {
-    int result;
-    long int i;
-    for( i = 0; i < MAX_HACKERS; i++ ) {
-	if( ( result = pthread_join( hackers[ i ], NULL ) ) ) {
-	    printf( "Erro em pthread_join do hacker[%ld]\n", i );
-	    exit( 0 );
+	int result;
+	long int i;
+	for( i = 0; i < MAX_HACKERS; i++ ) {
+		if( ( result = pthread_join( hackers[ i ], NULL ) ) ) {
+			printf( "Erro em pthread_join do hacker[%ld]\n", i );
+			exit( 0 );
+		}
 	}
-    }
-    for( i = 0; i < MAX_SERFS; i++ ) {
-	if( ( result = pthread_join( serfs[ i ], NULL ) ) ) {
-	    printf( "Erro em pthread_join do serf[%ld]\n", i );
-	    exit( 0 );
+	for( i = 0; i < MAX_SERFS; i++ ) {
+		if( ( result = pthread_join( serfs[ i ], NULL ) ) ) {
+			printf( "Erro em pthread_join do serf[%ld]\n", i );
+			exit( 0 );
+		}
 	}
-    }
 }
 
 void *doSomething( void *args ) {
-    pthread_mutex_lock( &mutex );
-    // Região critica
-    NUM_TEST++;
-    printf( "Ola %ld %d\n", (long int)args, NUM_TEST );
-    fflush( stdout );
-    pthread_mutex_unlock( &mutex );
-    return NULL;
+	pthread_mutex_lock( &mutex );
+	//Begin critical region
+
+	NUM_TEST++;
+	printf( "Ola %ld %d\n", (long int)args, NUM_TEST );
+	fflush( stdout );
+
+	//End critical region
+	pthread_mutex_unlock( &mutex );
+	return NULL;
 }
