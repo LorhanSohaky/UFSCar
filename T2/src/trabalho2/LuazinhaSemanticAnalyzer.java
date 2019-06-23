848a2f1b8e6e2f22695c278aec36fac4c2eebb7c
LorhanSohaky <lorhansohaky@gmail.com>
2019-06-23 15:44:12 -0300

    Comentando código

--- T2/src/trabalho2/LuazinhaSemanticAnalyzer.java
+++ T2/src/trabalho2/LuazinhaSemanticAnalyzer.java
@@ -16,50 +16,23 @@ public class LuazinhaSemanticAnalyzer extends LuazinhaBaseVisitor<Void> {
     public static String grupo = "740951 743602 743586";
 
     PilhaDeTabelas pilhaDeTabelas = new PilhaDeTabelas();
-
+    
+    /*
+    Adiciona ao escopo global.
+    */
     @Override
     public Void visitPrograma(LuazinhaParser.ProgramaContext ctx) {
         pilhaDeTabelas.empilhar(new TabelaDeSimbolos("global"));
-
-        // A chamada a seguir invoca o comportamento padrão,
-        // que é o de visitar todos os filhos
+        
         super.visitPrograma(ctx);
-        // Também poderia substituir por uma chamada específica a
-        // outro visitante, como a seguir:
-        //visitTrecho(ctx.trecho());
-        // Neste caso, é preciso especificar o contexto específico
-        // do visitante (trecho)
-
-
-        // Cuidado para lembrar de inserir corretamente as chamadas
-        // dos visitantes a seguir, pois no padrão Visitor do ANTLR,
-        // a visitação deve ser explicitamente controlada pelo programador.
-
+        
         pilhaDeTabelas.desempilhar();
-
-        // Deve haver "return null" no final de cada método, devido
-        // à verificação de tipos genéricos do Java. Como não estamos
-        // utilizando tipo de retorno, não é necessário.
         return null;
     }
 
-    // Não é necessário sobrescrever um determinado método visitante
-    // se a única coisa que ele faz é visitar os filhos, sem nenhuma
-    // ação adicional. O exemplo a seguir serve apenas para ilustrar
-    // esse efeito.
-//    @Override
-//    public Void visitBloco(LuazinhaParser.BlocoContext ctx) {
-//        // desnecessário, pois a única coisa que ele faz é visitar os filhos!
-//        super.visitBloco(ctx);
-//
-//        // desnecessário, pois a única coisa que ele faz é visitar os filhos
-//        // (obs, aqui chamando outro visitante ao invés do super)
-//        visitTrecho(ctx.trecho());
-//
-//        return null;
-//    }
-
-
+    /*
+    Adiciona a declaração da função ao escopo correto e se tiver ':' adiciona o parametro self
+    */
     @Override
     public Void visitComandoFunction(LuazinhaParser.ComandoFunctionContext ctx) {
         pilhaDeTabelas.empilhar(new TabelaDeSimbolos(ctx.nomedafuncao().nome));
@@ -90,7 +63,10 @@ public class LuazinhaSemanticAnalyzer extends LuazinhaBaseVisitor<Void> {
         
         return null;
     }
-
+    
+    /*
+    Declara as variáveis
+    */
     @Override
     public Void visitListavar(LuazinhaParser.ListavarContext ctx){
         for (String nome : ctx.nomes) {
@@ -102,6 +78,9 @@ public class LuazinhaSemanticAnalyzer extends LuazinhaBaseVisitor<Void> {
 
     }
 
+    /*
+    Declara lista de variáveis.
+    */
     @Override
     public Void visitListaParListaDeNomes(LuazinhaParser.ListaParListaDeNomesContext ctx){
        TabelaDeSimbolos tabela= pilhaDeTabelas.topo();
