859655c415546223c47e26a7c3fddd7aa9e57cf8
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-08-23 20:58:07 -0300

    Modificado layout e esquema de cores

--- 2019/CG/conversao.js
+++ 2019/CG/conversao.js
@@ -1,32 +1,69 @@
-let numero_colunas = 50;
-let numero_linhas = 50;
+const numberOfColumns  = 350;
+const numberOfLines = 350;
+const matriz=[];
+const colorsPalette = [ {"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},
+                            {"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},
+                            {"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},
+                            {"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},
+                            {"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},
+                            {"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},
+                            {"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},
+                            {"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255} ];
 
-let matriz=[];
+let dataToPlot = [];
 
-for (let i = 0; i < numero_linhas; i++) {
-    matriz.push([]);
-    for (let j = 0; j < numero_colunas; j++) {
-        matriz[i].push(0);
-    }
-}
+start();
 
-matriz[1][1] = 1;
-matriz[1][2] = 1;
+function start() {
+  createViewPortDataStructure();
 
-let table = "";
-
-for (let i = 0; i < numero_linhas; i++) {
-    table+="<tr>";
-    for (let j = 0; j < numero_colunas; j++) {
-        table+=`<td style="width:10px;height:10px;background-color:${matriz[i][j]?"black":"white"}"></td>`;
-    }
-    table+="</tr>";
+  setInterval(renderViewPort, 500);
 }
 
 
-let gerneratedHTML = `<table border="1">${table}</table>`;
+dataToPlot[1] = 100;
+dataToPlot[2] = 100;
+
+function createViewPortDataStructure() {
+  const numberOfPixels = numberOfLines * numberOfColumns;
+
+  for (let i = 0; i < numberOfPixels; i++) {
+      matriz[i] = 0;
+  }
+
+}
 
 
-let root = document.getElementById("root");
+function renderViewPort() {
+  const numberOfPixels = numberOfLines * numberOfColumns;
 
-root.innerHTML = gerneratedHTML;
+  for (let i = 0; i < 100; i++) {
+    matriz[Math.round(Math.random() * (numberOfPixels - 1))] = Math.round(Math.random() * 37);
+  }
+
+  let html = '<table cellpadding="0" cellspacing="0">';
+
+  for (let row = numberOfLines - 1; row >= 0; row--) {
+    html += '<tr>';
+
+    for (let column = numberOfColumns - 1; column >= 0; column--) {
+      const pixelIndex = column + ( numberOfColumns * row );
+      let color = colorsPalette[matriz[pixelIndex]];
+      //console.log(color);
+      //const colorString = `${color.r},${color.g},${color.b}`;
+
+      html += `<td class="pixel" style="background-color: rgb(${color})">`;
+      html += '</td>';
+    }
+
+    html += '</tr>';
+  }
+
+  html += '</table>';
+
+
+  let root = document.getElementById('root');
+
+  root.innerHTML = html;
+  //console.log('Renderizado!');
+}
