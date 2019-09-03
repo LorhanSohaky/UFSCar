615b7fc461416c3796730938130e2605efef2f41
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-09-03 11:36:55 -0300

    Criando meu shell

---
+++ 2019/SO2/aula4/my_shell.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(){
+	char command[2048];
+	pid_t forkStatus;
+
+        
+	while(1){
+		scanf(" %[^\n]",command);
+		if(strcmp(command,"q")==0){
+			break;
+		}else{
+			forkStatus = vfork();
+			if(forkStatus ==0){
+				char *end = strpbrk(command,"&");
+				if (end){
+					*end = '\0';
+				}
+				execlp(command,command, NULL);
+			}else if(forkStatus!=-1){
+				if(!strpbrk(command,"&")){
+					wait(NULL);
+				}
+			}
+		}
+
+	}
+	return 0;
+}
