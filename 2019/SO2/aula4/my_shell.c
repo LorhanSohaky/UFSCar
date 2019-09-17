96be85f1443bc706ab73257f15a24027a4d28dce
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-09-17 09:51:17 -0300

    Aceitando parâmetros ao chamar uma função

--- 2019/SO2/aula4/my_shell.c
+++ 2019/SO2/aula4/my_shell.c
@@ -1,37 +1,51 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void slice(char *args[], char *string) {
+  char *token;
+  int i;
 
-int main(){
-	char command[2048];
-	pid_t forkStatus;
+  for (token = strtok(string, " "), i = 0; token != NULL;
+       token = strtok(NULL, " "), i++) {
+    args[i] = token;
+  }
+  args[i] = NULL;
+}
 
-        
-	while(1){
-		printf("> ");
-		scanf(" %[^\n]",command);
-		if(strcmp(command,"q")==0){
-			break;
-		}else{
-			forkStatus = vfork();
-			if(forkStatus ==0){
-				char *end = strpbrk(command,"&");
-				if (end){
-					*end = '\0';
-				}
-				execlp(command,command, NULL);
-			}else if(forkStatus!=-1){
-				if(!strpbrk(command,"&")){
-					wait(NULL);
-				}
-			}
-		}
+int main() {
+  char command[2048];
+  pid_t forkStatus;
 
-	}
-	return 0;
+  char *args[2048];
+  int tem_e;
+
+  while (1) {
+    tem_e = 0;
+    printf("> ");
+    scanf(" %[^\n]", command);
+    if (strcmp(command, "q") == 0) {
+      break;
+    } else {
+      char *end = strrchr(command, '&');
+      if ('&' == command[strlen(command) - 1]) {
+        tem_e = 1;
+        *end = '\0';
+      }
+      forkStatus = vfork();
+      if (forkStatus == 0) {
+        slice(args, command);
+        execvp(command, args);
+      } else if (forkStatus != -1) {
+        if (!tem_e) {
+          wait(NULL);
+        }
+      }
+    }
+  }
+  return 0;
 }
