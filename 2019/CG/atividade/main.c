bfda0e38dbf9350a68117ea04e60d586b6e75437
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-11-29 09:23:01 -0300

    Iniciando atividade

--- 2019/CG/atividade/main.c
+++ 2019/CG/atividade/main.c
@@ -10,18 +10,11 @@
 */
 
 int main( void ) {
-	face *	f1, *f2, *f3, *f4, *f5;
-	object3d *objeto, *ob;
-	point3d * Normal, *ViewUp, *Observador;
-	object *  faces;
-
-	palette *	 palheta;
 	bufferdevice *dispositivo;
 	window *	  janela;
 	viewport *	porta;
 
-	float zcp, zpp;
-
+	palette *palheta;
 	palheta = CreatePalette( 6 );
 	SetColor( 0, 0, 0, palheta );
 	SetColor( 1, 1, 1, palheta );
@@ -30,65 +23,34 @@ int main( void ) {
 	SetColor( 0, 1, 0, palheta );
 	SetColor( 0, 0, 1, palheta );
 
-	f1 = CreateFace( 3 );
-	SetPointFace( 10.0, 10.0, 0.0, 5, f1 );
-	SetPointFace( 10.0, 0.0, 15.0, 5, f1 );
-	SetPointFace( 10.0, -10.0, 0.0, 5, f1 );
+	face *fQuadrado, *fTriangulo;
+	fQuadrado  = CreateFace( 4 );
+	fTriangulo = CreateFace( 3 );
+	SetPointFace( 1.0, 5.0, 0.0, 1, fQuadrado );
+	SetPointFace( 5.0, 5.0, 0.0, 1, fQuadrado );
+	SetPointFace( 5.0, 1.0, 0.0, 1, fQuadrado );
+	SetPointFace( 1.0, 1.0, 0.0, 1, fQuadrado );
 
-	f2 = CreateFace( 3 );
-	SetPointFace( -10.0, 10.0, 0.0, 5, f2 );
-	SetPointFace( -10.0, 0.0, 15.0, 5, f2 );
-	SetPointFace( -10.0, -10.0, 0.0, 5, f2 );
+	object *casa;
 
-	f3 = CreateFace( 4 );
-	SetPointFace( 10.0, 10.0, 0.0, 3, f3 );
-	SetPointFace( 10.0, 0.0, 15.0, 3, f3 );
-	SetPointFace( -10.0, 0.0, 15.0, 3, f3 );
-	SetPointFace( -10.0, 10.0, 0.0, 3, f3 );
+	object3d *objQuadrado, *objQuadradoBase;
+	objQuadrado = CreateObject3D( 1 );
+	SetObject3D( fQuadrado, objQuadrado );
 
-	f4 = CreateFace( 4 );
-	SetPointFace( 10.0, 0.0, 15.0, 4, f4 );
-	SetPointFace( 10.0, -10.0, 0.0, 4, f4 );
-	SetPointFace( -10.0, -10.0, 0.0, 4, f4 );
-	SetPointFace( -10.0, 0.0, 15.0, 4, f4 );
+	point3d *Normal, *ViewUp, *Observador;
 
-	f5 = CreateFace( 4 );
-	SetPointFace( 10.0, 10.0, 0.0, 5, f5 );
-	SetPointFace( 10.0, -10.0, 0.0, 5, f5 );
-	SetPointFace( -10.0, -10.0, 0.0, 5, f5 );
-	SetPointFace( -10.0, 10.0, 0.0, 5, f5 );
-
-	objeto = CreateObject3D( 5 );
-	SetObject3D( f1, objeto );
-	SetObject3D( f2, objeto );
-	SetObject3D( f3, objeto );
-	SetObject3D( f4, objeto );
-	SetObject3D( f5, objeto );
-
-	Normal	 = SetNormal( -1, 0, 0 );
+	Normal	 = SetNormal( 0, 0, 1 );
 	ViewUp	 = SetViewUp( 0, 1, 0 );
-	Observador = SetObserver( 5, 0, 0 );
+	Observador = SetObserver( 0, 0, 0 );
 
-	matrix3d *deslocamento = gerarMatrizDeDeslocamento( -50, 0, 0 );
-	TransformacaoLinear( deslocamento, objeto );
-	// Observador = LinearTransf3d(deslocamento, Observador);
+	objQuadradoBase = ConvertObjectBase( Normal, ViewUp, Observador, objQuadrado );
+	casa			= PerspProjFaces( objQuadradoBase, 0, -60 );
 
-	ob = ConvertObjectBase( Normal, ViewUp, Observador, objeto );
+	janela		= CreateWindow( -15, -15, 15, 15 );
+	dispositivo = CreateBuffer( 801, 601 );
+	porta		= CreateViewPort( 0, 0, 800, 600 );
 
-	// faces = ParalProjFaces(ob);
-	zpp   = 0.0;
-	zcp   = -60.0;
-	faces = PerspProjFaces( ob, zpp, zcp );
-
-	janela		= CreateWindow( -30, -30, 30, 30 );
-	dispositivo = CreateBuffer( 640, 480 );
-	porta		= CreateViewPort( 0, 0, 639, 479 );
-
-	DrawObject( &faces[0], janela, porta, dispositivo, 5 );
-	DrawObject( &faces[1], janela, porta, dispositivo, 5 );
-	DrawObject( &faces[2], janela, porta, dispositivo, 3 );
-	DrawObject( &faces[3], janela, porta, dispositivo, 5 );
-	DrawObject( &faces[4], janela, porta, dispositivo, 5 );
+	DrawObject( &casa[0], janela, porta, dispositivo, 1 );
 
 	Dump2PIPE( dispositivo, palheta );
 
