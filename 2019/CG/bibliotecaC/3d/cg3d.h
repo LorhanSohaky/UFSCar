baab916beccd30055761a5560d783466f8d157d3
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-11-29 08:37:01 -0300

    Código reformatado

--- 2019/CG/bibliotecaC/3d/cg3d.h
+++ 2019/CG/bibliotecaC/3d/cg3d.h
@@ -6,51 +6,45 @@ Universidade Federal de São Carlos
 
 #include "cg2d.h"
 
-typedef enum {X,Y,Z} EIXO;
+typedef enum { X, Y, Z } EIXO;
 
 typedef struct HPoint3D {
-  float x,
-        y,
-	z,
-	w;
-  int   color;
-  } point3d;
-      
+	float x, y, z, w;
+	int   color;
+} point3d;
+
 typedef struct Face {
-  int numbers_of_points;
-  point3d * points;
-  } face;
-  
+	int		 numbers_of_points;
+	point3d *points;
+} face;
+
 typedef struct Object3D {
-  int numbers_of_faces;
-  face * faces;
-  } object3d;
+	int   numbers_of_faces;
+	face *faces;
+} object3d;
 
 typedef struct HMatrix3D {
-  float a11, a12, a13, a14,
-        a21, a22, a23, a24,
-        a31, a32, a33, a34,
-	a41, a42, a43, a44;
-        } matrix3d;
+	float a11, a12, a13, a14, a21, a22, a23, a24, a31, a32, a33, a34, a41, a42, a43, a44;
+} matrix3d;
 
-point3d * VectorProduct3d(point3d *, point3d *);
-float Modulo3d(point3d *);
-point3d * Versor3d(point3d *);
-face * CreateFace(int);
-int SetPointFace(float, float, float, int, face *);
-point3d * SetNormal(float,float,float);
-point3d * SetViewUp(float,float,float);
-point3d * SetObserver(float,float,float);
-object3d * CreateObject3D(int);
-int SetObject3D(face *, object3d *);
-point3d * LinearTransf3d(matrix3d *, point3d *);
-object3d * ConvertObjectBase(point3d *, point3d *, point3d *, object3d *);
-object * ParalProjFaces(object3d *);
-object * PerspProjFaces(object3d *, float, float);
+point3d * VectorProduct3d( point3d *, point3d * );
+float	 Modulo3d( point3d * );
+point3d * Versor3d( point3d * );
+face *	CreateFace( int );
+int		  SetPointFace( float, float, float, int, face * );
+point3d * SetNormal( float, float, float );
+point3d * SetViewUp( float, float, float );
+point3d * SetObserver( float, float, float );
+object3d *CreateObject3D( int );
+int		  SetObject3D( face *, object3d * );
+point3d * LinearTransf3d( matrix3d *, point3d * );
+object3d *ConvertObjectBase( point3d *, point3d *, point3d *, object3d * );
+object *  ParalProjFaces( object3d * );
+object *  PerspProjFaces( object3d *, float, float );
 
-matrix3d *gerarMatrizDeDeslocamento(const float dX, const float dY, const float dZ);
-matrix3d *gerarMatrizDeEscala(const float sX, const float sY, const float sZ);
-matrix3d *gerarMatrizDeProjecaoPerspectiva(const float Zvp, const float Zcp);
-matrix3d *gerarMatrizDeRotacao(const float anguloEmGraus,const EIXO eixoDeRotacao);
+matrix3d *gerarMatrizDeDeslocamento( const float dX, const float dY, const float dZ );
+matrix3d *gerarMatrizDeEscala( const float sX, const float sY, const float sZ );
+matrix3d *gerarMatrizDeProjecaoPerspectiva( const float Zvp, const float Zcp );
+matrix3d *gerarMatrizDeRotacao( const float anguloEmGraus, const EIXO eixoDeRotacao );
 
-void TransformacaoLinear(matrix3d * W, object3d* objeto);
+void TransformacaoLinear( matrix3d *W, object3d *objeto );
--- 2019/CG/bibliotecaC/3d/exemplo_teste_3d.c
+++ 2019/CG/bibliotecaC/3d/exemplo_teste_3d.c
@@ -3,97 +3,94 @@
 /*
 	Para executar:
 	gcc -c cg2d.c
-        gcc -c cg3d.c
-        gcc exemplo_teste_3d.c -o exemplo_teste_3d cg3d.o cg2d.o -lm
+		gcc -c cg3d.c
+		gcc exemplo_teste_3d.c -o exemplo_teste_3d cg3d.o cg2d.o -lm
 	./exemplo_teste_3d&
-        ./view
+		./view
 */
 
-int main(void) {
- face * f1, * f2, * f3, * f4, * f5;
- object3d * objeto, * ob;
- point3d * Normal, * ViewUp, * Observador;
- object * faces;
- 
- palette * palheta;
- bufferdevice * dispositivo;
- window * janela;
- viewport * porta;
- 
- float zcp, zpp;
- 
- palheta = CreatePalette(6);
- SetColor(0,0,0,palheta);
- SetColor(1,1,1,palheta);
- SetColor(1,1,0,palheta);
- SetColor(1,0,0,palheta);
- SetColor(0,1,0,palheta);
- SetColor(0,0,1,palheta);
- 
- f1 = CreateFace(3);
- SetPointFace(10.0,10.0,0.0,5,f1);
- SetPointFace(10.0,0.0,15.0,5,f1);
- SetPointFace(10.0,-10.0,0.0,5,f1);
- 
- f2 = CreateFace(3);
- SetPointFace(-10.0,10.0,0.0,5,f2);
- SetPointFace(-10.0,0.0,15.0,5,f2);
- SetPointFace(-10.0,-10.0,0.0,5,f2);
- 
- f3 = CreateFace(4);
- SetPointFace(10.0,10.0,0.0,3,f3);
- SetPointFace(10.0,0.0,15.0,3,f3);
- SetPointFace(-10.0,0.0,15.0,3,f3);
- SetPointFace(-10.0,10.0,0.0,3,f3); 
+int main( void ) {
+	face *	f1, *f2, *f3, *f4, *f5;
+	object3d *objeto, *ob;
+	point3d * Normal, *ViewUp, *Observador;
+	object *  faces;
 
- f4 = CreateFace(4);
- SetPointFace(10.0,0.0,15.0,4,f4);
- SetPointFace(10.0,-10.0,0.0,4,f4);
- SetPointFace(-10.0,-10.0,0.0,4,f4);
- SetPointFace(-10.0,0.0,15.0,4,f4); 
+	palette *	 palheta;
+	bufferdevice *dispositivo;
+	window *	  janela;
+	viewport *	porta;
 
- f5 = CreateFace(4);
- SetPointFace(10.0,10.0,0.0,5,f5);
- SetPointFace(10.0,-10.0,0.0,5,f5);
- SetPointFace(-10.0,-10.0,0.0,5,f5);
- SetPointFace(-10.0,10.0,0.0,5,f5);
- 
- objeto = CreateObject3D(5);
- SetObject3D(f1,objeto);
- SetObject3D(f2,objeto);
- SetObject3D(f3,objeto);
- SetObject3D(f4,objeto);
- SetObject3D(f5,objeto);
+	float zcp, zpp;
 
+	palheta = CreatePalette( 6 );
+	SetColor( 0, 0, 0, palheta );
+	SetColor( 1, 1, 1, palheta );
+	SetColor( 1, 1, 0, palheta );
+	SetColor( 1, 0, 0, palheta );
+	SetColor( 0, 1, 0, palheta );
+	SetColor( 0, 0, 1, palheta );
 
- Normal = SetNormal(-1, 0, 0);
- ViewUp = SetViewUp(0, 1, 0);
- Observador = SetObserver(5, 0, 0);
+	f1 = CreateFace( 3 );
+	SetPointFace( 10.0, 10.0, 0.0, 5, f1 );
+	SetPointFace( 10.0, 0.0, 15.0, 5, f1 );
+	SetPointFace( 10.0, -10.0, 0.0, 5, f1 );
 
- matrix3d *deslocamento = gerarMatrizDeDeslocamento(-50, 0, 0);
- TransformacaoLinear(deslocamento, objeto);
- //Observador = LinearTransf3d(deslocamento, Observador);
+	f2 = CreateFace( 3 );
+	SetPointFace( -10.0, 10.0, 0.0, 5, f2 );
+	SetPointFace( -10.0, 0.0, 15.0, 5, f2 );
+	SetPointFace( -10.0, -10.0, 0.0, 5, f2 );
 
- 
- ob = ConvertObjectBase(Normal,ViewUp,Observador,objeto);
- 
- //faces = ParalProjFaces(ob);
- zpp = 0.0;
- zcp = -60.0;
- faces = PerspProjFaces(ob,zpp,zcp);
+	f3 = CreateFace( 4 );
+	SetPointFace( 10.0, 10.0, 0.0, 3, f3 );
+	SetPointFace( 10.0, 0.0, 15.0, 3, f3 );
+	SetPointFace( -10.0, 0.0, 15.0, 3, f3 );
+	SetPointFace( -10.0, 10.0, 0.0, 3, f3 );
 
- janela = CreateWindow(-30,-30,30,30);
- dispositivo = CreateBuffer(640,480);
- porta = CreateViewPort(0, 0, 639, 479);
- 
- DrawObject(&faces[0],janela,porta,dispositivo,5);
- DrawObject(&faces[1],janela,porta,dispositivo,5);
- DrawObject(&faces[2],janela,porta,dispositivo,3);
- DrawObject(&faces[3],janela,porta,dispositivo,5);
- DrawObject(&faces[4],janela,porta,dispositivo,5);
+	f4 = CreateFace( 4 );
+	SetPointFace( 10.0, 0.0, 15.0, 4, f4 );
+	SetPointFace( 10.0, -10.0, 0.0, 4, f4 );
+	SetPointFace( -10.0, -10.0, 0.0, 4, f4 );
+	SetPointFace( -10.0, 0.0, 15.0, 4, f4 );
 
- Dump2PIPE(dispositivo,palheta);
+	f5 = CreateFace( 4 );
+	SetPointFace( 10.0, 10.0, 0.0, 5, f5 );
+	SetPointFace( 10.0, -10.0, 0.0, 5, f5 );
+	SetPointFace( -10.0, -10.0, 0.0, 5, f5 );
+	SetPointFace( -10.0, 10.0, 0.0, 5, f5 );
 
- return 0; 
- }
+	objeto = CreateObject3D( 5 );
+	SetObject3D( f1, objeto );
+	SetObject3D( f2, objeto );
+	SetObject3D( f3, objeto );
+	SetObject3D( f4, objeto );
+	SetObject3D( f5, objeto );
 
+	Normal	 = SetNormal( -1, 0, 0 );
+	ViewUp	 = SetViewUp( 0, 1, 0 );
+	Observador = SetObserver( 5, 0, 0 );
+
+	matrix3d *deslocamento = gerarMatrizDeDeslocamento( -50, 0, 0 );
+	TransformacaoLinear( deslocamento, objeto );
+	// Observador = LinearTransf3d(deslocamento, Observador);
+
+	ob = ConvertObjectBase( Normal, ViewUp, Observador, objeto );
+
+	// faces = ParalProjFaces(ob);
+	zpp   = 0.0;
+	zcp   = -60.0;
+	faces = PerspProjFaces( ob, zpp, zcp );
+
+	janela		= CreateWindow( -30, -30, 30, 30 );
+	dispositivo = CreateBuffer( 640, 480 );
+	porta		= CreateViewPort( 0, 0, 639, 479 );
+
+	DrawObject( &faces[0], janela, porta, dispositivo, 5 );
+	DrawObject( &faces[1], janela, porta, dispositivo, 5 );
+	DrawObject( &faces[2], janela, porta, dispositivo, 3 );
+	DrawObject( &faces[3], janela, porta, dispositivo, 5 );
+	DrawObject( &faces[4], janela, porta, dispositivo, 5 );
+
+	Dump2PIPE( dispositivo, palheta );
+
+	return 0;
+}
