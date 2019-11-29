0e441db7fe63cd5ac4fec61a4f647ae81475eb6c
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-11-29 11:23:01 -0300

    Terminada a lateral do teto

--- 2019/CG/atividade/main.c
+++ 2019/CG/atividade/main.c
@@ -1,7 +1,8 @@
 /*
 Lorhan Sohaky
 
-Um gargalo é que a minha biblioteca substitui o objeto no momento em que aplica uma transformação
+- Tive que implementar uma função que aplica a transformação linear numa face
+- Um gargalo é que a minha biblioteca substitui o objeto no momento em que aplica uma transformação
 linear
 */
 
@@ -66,13 +67,15 @@ int main( void ) {
 	// DrawObject( &casa[1], janela, porta, dispositivo, 3 );
 
 	face *	f1, *f2, *f3, *f4, *f5;
-	matrix3d *m1, *m2, *m3, *m4, *m5, *m6;
+	matrix3d *m1, *m2, *m3, *m4, *m5, *m6, *m7;
 
 	m1 = gerarMatrizDeDeslocamento( 0, 0, 100 );
-	m2 = gerarMatrizDeEscala( 20, 1.1, 1 );
-	m3 = gerarMatrizDeRotacao( -22, X );
+	m2 = gerarMatrizDeEscala( 20, 1.15, 1 );
+	m3 = gerarMatrizDeRotacao( -22.1, X );
 	m4 = gerarMatrizDeRotacao( 90, Y );
-	m5 = gerarMatrizDeDeslocamento( -0.4, -5.15, 89.5 );
+	m5 = gerarMatrizDeDeslocamento( -0.3, -5.3, 89.5 );
+	m6 = gerarMatrizDeRotacao( 22.1, X );
+	m7 = gerarMatrizDeDeslocamento( -4.65, -5.3, 89.5 );
 
 	object *teto;
 
@@ -81,22 +84,28 @@ int main( void ) {
 
 	f2 = CreateFace( 3 );
 	criarTriangulo( f2 );
+	TransformacaoLinearFace( m1, f2 );
 
 	f3 = CreateFace( 4 );
 	criarQuadrado( f3 );
-
-	TransformacaoLinearFace( m1, f2 );
-
 	TransformacaoLinearFace( m2, f3 );
 	TransformacaoLinearFace( m3, f3 );
 	TransformacaoLinearFace( m4, f3 );
 	TransformacaoLinearFace( m5, f3 );
 
+	f4 = CreateFace( 4 );
+	criarQuadrado( f4 );
+	TransformacaoLinearFace( m2, f4 );
+	TransformacaoLinearFace( m6, f4 );
+	TransformacaoLinearFace( m4, f4 );
+	TransformacaoLinearFace( m7, f4 );
+
 	object3d *objTeto, *objTetoBase;
-	objTeto = CreateObject3D( 3 );
+	objTeto = CreateObject3D( 4 );
 	SetObject3D( f2, objTeto );
 	SetObject3D( f1, objTeto );
 	SetObject3D( f3, objTeto );
+	SetObject3D( f4, objTeto );
 
 	objTetoBase = ConvertObjectBase( Normal, ViewUp, Observador, objTeto );
 	teto		= PerspProjFaces( objTetoBase, 1, -60 );
@@ -104,6 +113,7 @@ int main( void ) {
 	DrawObject( &teto[0], janela, porta, dispositivo, 3 );
 	DrawObject( &teto[1], janela, porta, dispositivo, 5 );
 	DrawObject( &teto[2], janela, porta, dispositivo, 1 );
+	DrawObject( &teto[3], janela, porta, dispositivo, 1 );
 
 	Dump2PIPE( dispositivo, palheta );
 
