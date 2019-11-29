437a0d64eeaa62a4f0d6eaa931422bc3c74f8f29
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-11-29 09:25:07 -0300

    Reorganizando código

--- 2019/CG/atividade/main.c
+++ 2019/CG/atividade/main.c
@@ -1,18 +1,17 @@
 #include "cg3d.h"
 
-/*
-	Para executar:
-	gcc -c cg2d.c
-		gcc -c cg3d.c
-		gcc exemplo_teste_3d.c -o exemplo_teste_3d cg3d.o cg2d.o -lm
-	./exemplo_teste_3d&
-		./view
-*/
-
 int main( void ) {
 	bufferdevice *dispositivo;
 	window *	  janela;
 	viewport *	porta;
+	janela		= CreateWindow( -15, -15, 15, 15 );
+	dispositivo = CreateBuffer( 801, 601 );
+	porta		= CreateViewPort( 0, 0, 800, 600 );
+
+	point3d *Normal, *ViewUp, *Observador;
+	Normal	 = SetNormal( 0, 0, 1 );
+	ViewUp	 = SetViewUp( 0, 1, 0 );
+	Observador = SetObserver( 0, 0, 0 );
 
 	palette *palheta;
 	palheta = CreatePalette( 6 );
@@ -37,20 +36,10 @@ int main( void ) {
 	objQuadrado = CreateObject3D( 1 );
 	SetObject3D( fQuadrado, objQuadrado );
 
-	point3d *Normal, *ViewUp, *Observador;
-
-	Normal	 = SetNormal( 0, 0, 1 );
-	ViewUp	 = SetViewUp( 0, 1, 0 );
-	Observador = SetObserver( 0, 0, 0 );
-
 	objQuadradoBase = ConvertObjectBase( Normal, ViewUp, Observador, objQuadrado );
 	casa			= PerspProjFaces( objQuadradoBase, 0, -60 );
 
-	janela		= CreateWindow( -15, -15, 15, 15 );
-	dispositivo = CreateBuffer( 801, 601 );
-	porta		= CreateViewPort( 0, 0, 800, 600 );
-
-	DrawObject( &casa[0], janela, porta, dispositivo, 1 );
+	DrawObject( &casa[0], janela, porta, dispositivo, 3 );
 
 	Dump2PIPE( dispositivo, palheta );
 
